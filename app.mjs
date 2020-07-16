4d4464a9810ca8f229c9da1abc649e7b7143c26e
danielotieno <danielotieno.ke@gmail.com>
2020-07-16 16:00:48 +0300

    chore:setup view engine

--- app.mjs
+++ app.mjs
@@ -1,5 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import morgan from 'morgan';
+import exphbs from 'express-handlebars';
 import connectDB from './config/db.mjs';
 
 // Load Global configs
@@ -8,6 +10,15 @@ dotenv.config({ path: './config/config.env' });
 connectDB();
 
 const app = express();
+
+// Logging
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+
+// Setup Handlebars Engine
+app.engine('.hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }));
+app.set('view engine', '.hbs');
 
 app.listen();
 
